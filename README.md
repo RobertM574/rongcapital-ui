d46bbae08f27a526f34801a40c3c009a590ba56c
nix <xiaoyao.ning@gmail.com>
2017-06-30 23:09:35 +0800

    调整项目配置

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Rongcapital UI
 
-npm run dev 生成开发版本
-npm run release 生成生产版本
+[![Build Status](https://travis-ci.org/WellerQu/rongcapital-ui.svg?branch=master)](https://travis-ci.org/WellerQu/rongcapital-ui)
+
+`npm run release:dev` 生成开发版本
+`npm run release` 生成生产版本
+`npm run dev` 开始开发
 
 http://airbnb.io/enzyme/docs/api/index.html
 https://storybook.js.org/
--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,22 +1,33 @@
 // for storybook
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const WebpackShellPlugin = require('webpack-shell-plugin');
 const path = require('path');
+const os = require('os');
 
 // load the default config generator.
 const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config');
 const customConfig = require('./webpack.config.base');
 
+const isWindows = os.platform() === 'win32';
+
 module.exports = (baseConfig, env) => {
     const config = genDefaultConfig(baseConfig, env);
     // Extend it as you need.
 
-    console.dir(customConfig.module.rules[2])
     // For load sass
     config.module.rules.push({
         test: /\.sass$/,
         loaders: ["style-loader", "css-loader", "sass-loader"],
     });
+
+    // For automatic open default Broswer
+    config.plugins.push(
+        new WebpackShellPlugin({
+            onBuildStart: [],
+            onBuildEnd: [`${isWindows?'start':'open'} http://localhost:9001`]
+        })
+    );
 
     return config;
 };
