0d37d75d10d75916afe20d1c7aaede0b1bb73354
nix <xiaoyao.ning@gmail.com>
2017-07-10 12:49:31 +0800

    修复测试用例无法通过的问题

--- test/core/navigationView.spec.js
+++ test/core/navigationView.spec.js
@@ -2,13 +2,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import { shallow, render } from 'enzyme';
 
 import { NavigationView } from '../../src';
 
 import * as componentStyles from '../../src/styles/navigationView.sass';
 
-describe.skip('core component navigationView', () => {
+const ItemView = ({ children, next, prev }) => (
+    <div className="navItem">
+        <p>{ children }</p>
+        <div>
+            <button onClick={ prev }>Prev</button>
+            <button onClick={ next }>Next</button>
+        </div>
+    </div>
+);
+
+ItemView.propTypes = {
+    children: PropTypes.string,
+    prev: PropTypes.func,
+    next: PropTypes.func,
+};
+
+describe.only('core component navigationView', () => {
 
     it('initialize by default', () => {
         const wrapper = shallow(<NavigationView />);
@@ -16,28 +32,13 @@ describe.skip('core component navigationView', () => {
     });
 
     it('initialize by 1 child', () => {
-        const ItemView = ({ children, next, prev }) => (
-            <div>
-                <p>{ children }</p>
-                <div>
-                    <button onClick={ prev }>Prev</button>
-                    <button onClick={ next }>Next</button>
-                </div>
-            </div>
-        );
-
-        ItemView.propTypes = {
-            children: PropTypes.string,
-            prev: PropTypes.func,
-            next: PropTypes.func,
-        };
-
-        const wrapper = shallow(
+        const wrapper = render(
             <NavigationView width={ 200 } height={ 100 }>
                 <ItemView>对饮成三人</ItemView>
             </NavigationView>
         );
-        expect(wrapper.find('navItem')).to.have.lengthOf(1);
-        expect(wrapper.find('navItem').children('p').text()).to.equal('对饮成三人');
+
+        expect(wrapper.children('div')).to.have.lengthOf(1);
+        expect(wrapper.children('div').text()).to.equal('对饮成三人PrevNext');
     });
 });
